b419169c24ef541d5843c56516bc25bc4e1d3a4d
Maddy Tan <maddytan7@gmail.com>
2022-06-29 18:12:13 +0000

    includes dashboard counts

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,9 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with :name => ENV["USERNAME"], :password => ENV["PASSWORD"]
+  
   def show
+    @products = Product.order(id: :desc).all
+    @categories = Category.all
   end
+
 end
