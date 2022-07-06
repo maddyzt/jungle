76bf4482c803028c450f20dbce35aedb0e45d16f
Maddy Tan <maddytan7@gmail.com>
2022-07-06 14:53:45 +0000

    updates to cypress

--- cypress/integration/add_to_cart.spec.js
+++ cypress/integration/add_to_cart.spec.js
@@ -2,11 +2,11 @@ describe("Add to Cart", () => {
   it("should navigate to home", () => {
     cy.visit("/");
   })
-  it ("should click on a product's add to cart", () => {
-    cy.contains("Scented Blade").parent('article').find(".btn").click()
+  it("should click on a product's add to cart", () => {
+    cy.contains("Scented Blade").parent('article').find(".btn").click({ force: true })
 
   })
-  it ("should increment by 1", () => {
+  it("should increment by 1", () => {
     cy.contains("My Cart (1)").should("exist")
   })
 });
